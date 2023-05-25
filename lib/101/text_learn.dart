285c125eb3b801fe04ca62c208fa9d05db4bd0e2
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-05-25 21:08:31 +0300

    devammm

---
+++ lib/101/text_learn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextLearnView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text(
+        "Buy  the best one",
+      style:TextStyle(
+        wordSpacing:2
+      ),
+      ),),
+    );
+  }
+}
